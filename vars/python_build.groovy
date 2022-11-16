800b1074a3b00c1062bc96434e447127e126b5de
Raihan Kheraj <65919439+raihankheraj@users.noreply.github.com>
2022-11-15 22:01:51 -0800

    Update python_build.groovy

---
+++ vars/python_build.groovy
@@ -0,0 +1,12 @@
+def call() {
+    pipeline {
+        agent any
+        stages {
+            stage('build') {
+                steps {
+                    sh 'pip install -r requirements.txt'
+                }
+            }
+        }
+    }
+}
