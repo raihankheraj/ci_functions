3dc32c41ab5ca4c1f188a10b0e1f46aecad8dff4
Raihan Kheraj <65919439+raihankheraj@users.noreply.github.com>
2022-11-16 19:14:21 -0800

    add lint stage

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -7,6 +7,12 @@ def call(service) {
                     sh "pip install -r ${service}/requirements.txt"
                 }
             }
+            stage('Python Lint') {
+                steps {
+                    sh 'pylint-fail-under --fail_under 5.0 *.py'
+                }
+            }
+
         }
     }
 }
