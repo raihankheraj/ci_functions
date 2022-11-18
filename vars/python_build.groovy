f6c223a3104f10a6b4c1022468f905ce739ad683
Raihan Kheraj <65919439+raihankheraj@users.noreply.github.com>
2022-11-18 12:49:13 -0800

    Update python_build.groovy

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -1,4 +1,4 @@
-def call(service) {
+def call(service, dockerRepoName, imageName) {
     pipeline {
         agent any
         stages {
@@ -12,6 +12,20 @@ def call(service) {
                     sh "pylint-fail-under --fail_under 5.0 ${service}/app.py"
                 }
             }
+
+            stage('Package') {
+                when {
+                    expression { env.GIT_BRANCH == 'origin/main' }
+                }
+                steps {
+                    withCredentials([string(credentialsId: 'DockerHub', variable: 'TOKEN')]) {
+                        sh "docker login -u 'raihankheraj' -p '$TOKEN' docker.io"
+                        sh "docker build -t ${dockerRepoName}:latest --tag raihankheraj/${dockerRepoName}:${imageName} ."
+                        sh "docker push raihankheraj/${dockerRepoName}:${imageName}"
+                    }
+                }
+            }
+
 
         }
     }
